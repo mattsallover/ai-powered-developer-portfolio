4978d8471c5614620a49d4e5cd985dd84ac53a67
mattsallover <levine1520@gmail.com>
2025-10-24 19:56:33 -0500

    Add intersection diagram showing Matt's unique position at the crossroads of business, AI, music, and creative technology

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import IntersectionDiagram from './components/IntersectionDiagram';
 import Projects from './components/Projects';
 import Chatbot from './components/Chatbot';
 import Footer from './components/Footer';
@@ -12,6 +13,7 @@ const App: React.FC = () => {
       <Header />
       <main className="container mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
         <Hero />
+        <IntersectionDiagram />
         <Projects />
       </main>
       <Chatbot />
